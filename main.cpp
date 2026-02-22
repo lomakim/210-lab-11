6bc08019926e0c5de23ba512256bdde9c7de2f1b
lomakim <lomakim@gmail.com>
2026-02-22 13:10:41 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
 // COMSC-210 | Lab 11 | Loma Kim
+#include <iostream>
+#include <string>
+#include <array>
+using namespace std;
+
+/* Struct Declaration for video game club member data. 
+Includes: name, phone number, favorite game overall, rating for their
+favorite game, and an array of games they have played in the last month. */
+struct VideoGameClub {
+    string name, phoneNumber, favGame;
+    double favRating;
+    string * recentGames;
+};
+
+int main() {
+
+    return 0;
+}
